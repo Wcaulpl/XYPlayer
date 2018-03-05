ea537cedd6b7be2150b62c1a045caa362982e981
Wcaulpl <Wcaulpl@WcaulpldeMacBook-Pro.local>
2018-03-05 10:28:29 +0800

    创建工程

---
+++ XYPlayer/XYPlayer/XYFloderViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPFloderViewController.h
+//  MobliePlayer
+//
+//  Created by zyyt on 17/4/24.
+//  Copyright © 2017年 conglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYFloderViewController : UIViewController
+
+@end
---
+++ XYPlayer/XYPlayer/XYFloderViewController.m
@@ -0,0 +1,40 @@
+//
+//  MPFloderViewController.m
+//  MobliePlayer
+//
+//  Created by zyyt on 17/4/24.
+//  Copyright © 2017年 conglei. All rights reserved.
+//
+
+#import "XYFloderViewController.h"
+
+@interface XYFloderViewController ()// <UITableViewDataSource,UITableViewDelegate>
+@property (strong,nonatomic) UITableView *tableView;
+/** 数据 */
+@property (strong,nonatomic) NSMutableArray *dataArray;
+@end
+
+@implementation XYFloderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
